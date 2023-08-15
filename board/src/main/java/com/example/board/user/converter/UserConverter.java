a414b4f256aff0c38ef6911ed431f196460258f1
mywnajsldkf <mywnajsldkf@gmail.com>
2023-08-16 01:32:52 +0900

    Feat : userEntity -> userInfoResponse converter 구현 #1

--- board/src/main/java/com/example/board/user/converter/UserConverter.java
+++ board/src/main/java/com/example/board/user/converter/UserConverter.java
@@ -2,6 +2,7 @@ package com.example.board.user.converter;
 
 import com.example.board.user.entity.UserEntity;
 import com.example.board.user.model.request.UserCreateRequest;
+import com.example.board.user.model.response.UserInfoResponse;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -14,4 +15,14 @@ public class UserConverter {
                 build();
     }
 
+    public static UserInfoResponse from(UserEntity userEntity) {
+        if (userEntity == null) {
+            return null;
+        }
+
+        return UserInfoResponse.builder().
+                email(userEntity.getEmail()).
+                password(userEntity.getPassword()).
+                build();
+    }
 }
