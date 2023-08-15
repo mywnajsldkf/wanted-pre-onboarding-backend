2d073c753cd2014c06d9f148a27726c54fe6a57b
mywnajsldkf <mywnajsldkf@gmail.com>
2023-08-16 01:37:55 +0900

    Feat : User 생성 API 추가 #1

---
+++ board/src/main/java/com/example/board/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.board.user.controller;
+
+import com.example.board.support.ApiResponse;
+import com.example.board.support.ApiResponseGenerator;
+import com.example.board.support.MessageCode;
+import com.example.board.user.model.request.UserCreateRequest;
+import com.example.board.user.model.response.UserInfoResponse;
+import com.example.board.user.service.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserServiceImpl userService;
+
+    @PostMapping()
+    @ResponseBody
+    public ApiResponse<ApiResponse.SuccessBody<UserInfoResponse>> createUser(@RequestBody UserCreateRequest request) {
+        return ApiResponseGenerator.success(userService.createUser(request), HttpStatus.CREATED, MessageCode.RESOURCE_CREATED);
+    }
+}
