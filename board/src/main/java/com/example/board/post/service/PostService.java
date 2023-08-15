48d26ce9acd82f9ee0eaec168cec9270b4ec1f9f
mywnajsldkf <mywnajsldkf@gmail.com>
2023-08-16 06:04:14 +0900

    Feat : 게시글 정보 반환 API 구현 #5

--- board/src/main/java/com/example/board/post/service/PostService.java
+++ board/src/main/java/com/example/board/post/service/PostService.java
@@ -2,7 +2,9 @@ package com.example.board.post.service;
 
 import com.example.board.post.model.request.PostCreateRequest;
 import com.example.board.post.model.response.PostInfoResponse;
+import org.springframework.data.domain.Page;
 
 public interface PostService {
     PostInfoResponse createPost(PostCreateRequest postCreateRequest);
+    Page<PostInfoResponse> findAllPost(Integer page, Integer size);
 }
