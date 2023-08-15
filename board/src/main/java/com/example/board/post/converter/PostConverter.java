5ccc6cfdaac22c61d561c8d1de4b1fbc8ab084eb
mywnajsldkf <mywnajsldkf@gmail.com>
2023-08-16 04:08:27 +0900

    Feat : 게시글 변환 기능 구현 #5

---
+++ board/src/main/java/com/example/board/post/converter/PostConverter.java
@@ -0,0 +1,28 @@
+package com.example.board.post.converter;
+
+import com.example.board.post.entity.PostEntity;
+import com.example.board.post.model.request.PostCreateRequest;
+import com.example.board.post.model.response.PostInfoResponse;
+
+public class PostConverter {
+    public static PostEntity to(PostCreateRequest postCreateRequest) {
+        return PostEntity.builder().
+                title(postCreateRequest.getTitle()).
+                content(postCreateRequest.getContent()).
+                userId(postCreateRequest.getUserId()).
+                build();
+    }
+
+    public static PostInfoResponse from(PostEntity postEntity) {
+        if (postEntity == null) {
+            return null;
+        }
+
+        return PostInfoResponse.builder().
+                title(postEntity.getTitle()).
+                content(postEntity.getContent()).
+                userId(postEntity.getUserId()).
+                build();
+
+    }
+}
