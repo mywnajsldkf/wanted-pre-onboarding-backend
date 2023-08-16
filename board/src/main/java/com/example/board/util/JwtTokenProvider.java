6a18f3fa03cc4195adba97ef82fbb897a3f919bd
mywnajsldkf <mywnajsldkf@gmail.com>
2023-08-16 13:20:53 +0900

    Feat : JwtToken 생성자 클래스 #10

---
+++ board/src/main/java/com/example/board/util/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package com.example.board.util;
+
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.BasicJsonParser;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${jwt.password}")
+    private String secretKey;
+
+    public String createAuthToken(String email) {
+        return create(email, "authToken", 1);
+    }
+
+    public String createRefreshToken(String email) {
+        return create(email, "refreshToken", 2);
+    }
+
+    private String create(String email, String subject, Integer hours) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + hours * Duration.ofHours(4).toMillis());
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setSubject(subject)
+                .claim("email", email)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encodeToString(secretKey.getBytes()))
+                .compact();
+    }
+
+    public Integer parseJwtToken(String token) {
+        String payloadJWT = token.split("\\.")[1];
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+
+        final String payload = new String(decoder.decode(payloadJWT));
+        JsonParser jsonParser = new BasicJsonParser();
+        Map<String, Object> jsonArray = jsonParser.parseMap(payload);
+        Integer userId = Integer.valueOf(jsonArray.get("email").toString());
+
+        return userId;
+    }
+
+    private String removeBearer(String token) {
+        return token.substring("Bearer".length());
+    }
+}
